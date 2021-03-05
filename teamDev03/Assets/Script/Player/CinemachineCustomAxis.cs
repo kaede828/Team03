6b1c37e886d7cd79179585918c2cba8ca3187ee3
koiso9758 <koiso9758@gmail.com>
2021-03-05 14:40:07 +0900

    Cinemachineカメラ追加

---
+++ teamDev03/Assets/Script/Player/CinemachineCustomAxis.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CinemachineCustomAxis : MonoBehaviour
+{
+    public bool xInversion, yInversion;
+    void Start()
+    {
+        Cinemachine.CinemachineCore.GetInputAxis = GetAxisCustom;
+    }
+    public float GetAxisCustom(string axisName)
+    {
+        if (axisName == "Mouse X")
+        {
+            return Input.GetAxis(axisName) * (xInversion ? -1f : 1f);
+        }
+        else if (axisName == "Mouse Y")
+        {
+            return Input.GetAxis(axisName) * (yInversion ? -1 : 1);
+        }
+        return 0;
+    }
+}
