7c3fcbea44d6fb92f0cd2ebcdd3da591ce524de2
kaede828 <kaede1999828@gmail.com>
2021-03-08 14:55:05 +0900

    カメラ反転修正

---
+++ teamDev03/Assets/Script/Inverse.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inverse : MonoBehaviour
+{
+    public bool xInversion, yInversion;
+
+    void Start()
+    {
+        Cinemachine.CinemachineCore.GetInputAxis = GetAxisCustom;
+    }
+
+    public float GetAxisCustom(string axisName)
+    {
+        if (axisName == "Mouse X")
+        {
+            return Input.GetAxis(axisName) * (xInversion ? -1f : 1f);
+        }
+        else if (axisName == "Mouse Y")
+        {
+            return Input.GetAxis(axisName) * (yInversion ? -1 : 1);
+        }
+
+        return 0;
+    }
+}
