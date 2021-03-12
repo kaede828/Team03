57147e040094d67f39008d78a19673a65f4eb3b4
kaede828 <kaede1999828@gmail.com>
2021-03-12 16:17:10 +0900

    ゲームフローのひな型プログラム、ライトの位置調整

---
+++ teamDev03/Assets/Scripts/State.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class State : MonoBehaviour
+{
+    private bool bossState;
+    private float gameTime;
+
+    public GameObject boss;
+    public GameObject attack;
+
+    private MeshRenderer mesh;
+    private BoxCollider col;
+    private MeshRenderer attackMesh;
+    private BoxCollider attackCol;
+
+    enum Game_Type
+    {
+        real = 0,
+        dreame = 1,
+        interval = 2,
+    }
+
+    Game_Type type;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        type = Game_Type.real;
+        //falseの時は見えない、trueの時は見える
+        bossState = false;
+        gameTime = 0;
+        mesh = boss.GetComponent<MeshRenderer>();
+        col = boss.GetComponent<BoxCollider>();
+        attackMesh = attack.GetComponent<MeshRenderer>();
+        attackCol = attack.GetComponent<BoxCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameTime += Time.deltaTime;
+
+        switch (type)
+        {
+            case Game_Type.real:
+                bossState = false;
+
+                //メッシュレンダラー、当たり判定のON/OFF
+                mesh.enabled = false;
+                col.enabled = false;
+                attackMesh.enabled = false;
+                attackCol.enabled = false;
+                break;
+
+            case Game_Type.dreame:
+                bossState = true;
+
+                //メッシュレンダラー、当たり判定のON/OFF
+                mesh.enabled = true;
+                col.enabled = true;
+                attackMesh.enabled = true;
+                attackCol.enabled = true;
+                break;
+
+            case Game_Type.interval:
+                bossState = true;
+
+                //メッシュレンダラー、当たり判定のON/OFF
+                mesh.enabled = true;
+                col.enabled = true;
+                attackMesh.enabled = true;
+                attackCol.enabled = true;
+                break;
+        }
+
+        if (gameTime <= 60)
+        {
+            type = Game_Type.real;
+            Debug.Log("現実");
+        }
+
+        if (gameTime >= 60)
+        {
+            type = Game_Type.dreame;
+            Debug.Log("夢");
+        }
+
+        if (gameTime >= 120)
+        {
+            type = Game_Type.interval;
+            Debug.Log("狭間");
+            gameTime = 0;
+        }
+    }
+}
